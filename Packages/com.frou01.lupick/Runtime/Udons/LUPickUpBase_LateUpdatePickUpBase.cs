8d5cb1c9b0ec93ae6224b53a4366421c9401d06c
frou01 <frou01stg44hurutaka@gmail.com>
2026-02-15 22:45:27 +0900

    change sync call timing for edit syncing data on subclass

--- Packages/com.frou01.lupick/Runtime/Udons/LUPickUpBase_LateUpdatePickUpBase.cs
+++ Packages/com.frou01.lupick/Runtime/Udons/LUPickUpBase_LateUpdatePickUpBase.cs
@@ -119,6 +119,7 @@ public class LUPickUpBase_LateUpdatePickUpBase : UdonSharpBehaviour
             {
                 //Debug.Log("onDropInit");
                 onDropInit_OwnerOnly();
+                RequestSerialization();
             }
             else if (dropFlag)
             {
@@ -174,11 +175,13 @@ public class LUPickUpBase_LateUpdatePickUpBase : UdonSharpBehaviour
             {
                 //Debug.Log("onPickInit");
                 onPickInit_OwnerOnly();
+                RequestSerialization();
             }
             MoveObjectByTrackingData();
         }
         else
         {
+            //Debug.Log("LUPickUpBase_LateUpdatePickUpBase debug onPicked OnRemote");
             MoveObjectByBone();
         }
         CalculateOffsetOnTransform(TransformCache.parent);
@@ -189,7 +192,6 @@ public class LUPickUpBase_LateUpdatePickUpBase : UdonSharpBehaviour
         MoveObjectByOnTransformOffset(TransformCache.parent);
         CalculateOffsetOnTrackingData();
         CalculateOffsetOnBone();
-        RequestSerialization();
         if (!LocalPlayer.IsUserInVR()) SendCustomEventDelayedSeconds(nameof(DeskTopWalkAround), 2);
         pickInitFlag = false;
     }
@@ -199,7 +201,6 @@ public class LUPickUpBase_LateUpdatePickUpBase : UdonSharpBehaviour
         FetchTrackingData(ownerPlayer);
         MoveObjectByTrackingData();
         CalculateOffsetOnTransform(TransformCache.parent);
-        RequestSerialization();
         dropInitFlag = false;
     }
     protected virtual void onDropped_OwnerOnly()
